a79fbb738dc7387c2e8c7a61856931db310f3366
Eric <eric.berkovsky@yahoo.com>
2021-11-29 00:47:31 -0800

    Added config file for user's to input their data

--- zendesk/config.py
+++ zendesk/config.py
@@ -2,4 +2,4 @@ django_secret_key = 'enter_your_secret_key'
 
 subdomain = 'enter_your_subdomain'
 
-credentials = 'enter_your_email_address' + '/token', 'enter_your_api_token'
+credentials = 'enter_your_email_address' + '/token', 'enter_api_token'
